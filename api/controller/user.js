5242c67e0adf276b93d39a9199e42e6a89adfc21
LaisFSGomes <laisgomes@alu.ufc.br>
2022-12-17 18:02:33 -0300

    chore: criando as funções de add, updade e delete

--- api/controller/user.js
+++ api/controller/user.js
@@ -7,3 +7,42 @@ export const getUsers = (_, res) => {
         return res.status(200).json(data);
     });
 };
+
+export const addUser = (req, res) => {
+    const q = "INSERT INTO usuarios (´nome´, ´email´, ´fone´, ´data_nascimento´) VALUES (?)";
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento
+    ];
+
+    bd.query(q, [values], (err)=> {
+        if (err) return res.json(err);
+        return res.status(200).json("usuario adicionado com sucesso");
+    });
+};
+
+export const updateUser = (req, res) => {
+    const q = "UPDATE usuarios SET nome = ?, email = ?, fone = ?, data_nascimento = ? WHERE id = ?";
+    const values = [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nascimento,
+    ];
+
+    bd.query(q, [...values, req.params.id], (err) => {
+        if (err) return res.json(err);
+        return res.status(200).json("usuario atualizado com sucesso");
+    });
+};
+
+export const deleteUser = (req, res) => {
+    const q = "DELETE FROM usuarios WHERE id = ?";
+    
+    bd.query(q, [req.params.id], (err) => {
+        if (err) return res.json(err);
+        return res.status(200).json("usuario deletado com sucesso");
+    });
+};
